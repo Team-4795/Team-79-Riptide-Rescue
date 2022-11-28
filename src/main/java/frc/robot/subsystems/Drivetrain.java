48aa4064b4c4061dba52bb09cb5caaadbdba9477
Halil <75945892+AI-HBB@users.noreply.github.com>
2022-11-28 18:03:19 -0500

    Possibly not working version

---
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -0,0 +1,37 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.motorcontrol.PWMTalonSRX;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class Drivetrain extends SubsystemBase {
+
+  // TO-DO Create two PWMTalonSRX motor controllers
+  private final PWMTalonSRX m_leftMotor = new PWMTalonSRX(5);
+  private final PWMTalonSRX m_rightMotor = new PWMTalonSRX(6);
+
+  // TO-DO Set up the differential drive controller
+  private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
+
+
+  /** Creates a new Drivetrain. */
+  public Drivetrain() {
+    // We need to invert one side of the drivetrain so that positive voltages
+    // result in both sides moving forward. Depending on how your robot's
+    // gearbox is constructed, you might have to invert the left side instead.
+  }
+
+  public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
+    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+    //TO-DO Call the arcade drive method
+  }
+
+  @Override
+  public void periodic() {
+    // This method will be called once per scheduler run
+  }
+}
