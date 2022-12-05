25b15d495534cbebdcc4831444c5fd745c25450c
Halil <hbbattal2006@gmail.com>
2022-12-05 17:18:30 -0500

    Test

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -12,8 +12,8 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class Drivetrain extends SubsystemBase {
 
   // TO-DO Create two PWMTalonSRX motor controllers
-  private final PWMVictorSPX m_leftMotor = new PWMVictorSPX(5);
-  private final PWMVictorSPX m_rightMotor = new PWMVictorSPX(6);
+  private final PWMVictorSPX m_leftMotor = new PWMVictorSPX(6);
+  private final PWMVictorSPX m_rightMotor = new PWMVictorSPX(5);
 
   
   // TO-DO Set up the differential drive controller
@@ -22,13 +22,19 @@ public class Drivetrain extends SubsystemBase {
 
   /** Creates a new Drivetrain. */
   public Drivetrain() {
+    //m_leftMotor.setInverted(true);
+
     // We need to invert one side of the drivetrain so that positive voltages
     // result in both sides moving forward. Depending on how your robot's
     // gearbox is constructed, you might have to invert the left side instead.
   }
 
   public void arcadeDrive(double xaxisSpeed, double zaxisRotate) {
-    m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
+     if(zaxisRotate<0) m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate+0.1);
+     if(zaxisRotate>0) m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate-0.1);
+
+
+    //m_diffDrive.arcadeDrive(xaxisSpeed, zaxisRotate);
     //TO-DO Call the arcade drive method
   }
 
